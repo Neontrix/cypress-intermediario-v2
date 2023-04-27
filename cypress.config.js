493f8ae0ceba2500d94a6706ef50edb720780bae
Your Name <vitor.sos.2010@gmail.com>
2023-04-27 15:30:57 -0300

    Testes de API e aproveitamento de API para performance nos testes GUI

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
+    env: {
+      hideCredentials: true,
+      //Feedback visual API (poderia utilizar cy.api ao invés de cy.request e não precisaria do requestMode declarado como true)
+      requestMode: true,
+    },
   },
   fixturesFolder: false,
   video: false,
